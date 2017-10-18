748e4c538d6102806fa1611ae4395e15acdfd38d
Your Name <you@example.com>
2017-10-17 17:03:44 -0700

    created JSON, working prompt for initial commands, added gitignore

---
+++ basicFlashcard.js
@@ -0,0 +1,21 @@
+var Basic = function(front, back) {
+    this.front = front;
+    this.back = back;
+
+};
+
+Basic.prototype.printCard = function() {
+    console.log('Front: ' + this.front + ', ' + 'Back: ' + this.back);
+};
+
+Basic.prototype.printFront = function() {
+    console.log(this.front);
+
+};
+
+
+Basic.prototype.printBack = function() {
+    console.log('Nice try! The answer is ' + this.back + '.');
+};
+
+module.exports = Basic;
