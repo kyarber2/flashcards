2f946bc7a844b137152b3df9e4c9939698c2486b
Your Name <you@example.com>
2017-10-19 10:11:08 -0700

    working flip function, code is accepting user answer, but not yet comparing with currentCard

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var basicCard = require("./basicFlashcard.js");
 var correct = 0;
 var wrong = 0;
 var deck = [];
+var currentCard = '';
+var userGuess = ''.toLowerCase();
 
 var musicSources = [{'front':'What word is both a fruit and a color?', 'back':'orange'}, {'front':'Which band has an album named "Rubber Soul?"', 'back':'The Beatles'}, {'front':'Who stars in "The Hunger Games"?', 'back':'Jennifer Lawrence'}, {'front':'What city is the capital of Ecuador?', 'back':'Quito'}];
 
@@ -14,7 +16,7 @@ var parksSources = [{'front':'What word is both a fruit and a color?', 'back':'o
 
 var capitalSources = [{'front':'What word is both a fruit and a color?', 'back':'orange'}, {'front':'Which band has an album named "Rubber Soul?"', 'back':'The Beatles'}, {'front':'Who stars in "The Hunger Games"?', 'back':'Jennifer Lawrence'}, {'front':'What city is the capital of Ecuador?', 'back':'Quito'}];
 
-var moviesSources = [{'front':'What word is both a fruit and a color?', 'back':'orange'}, {'front':'Which band has an album named "Rubber Soul?"', 'back':'The Beatles'}, {'front':'Who stars in "The Hunger Games"?', 'back':'Jennifer Lawrence'}, {'front':'What city is the capital of Ecuador?', 'back':'Quito'}];
+var moviesSources = [{'front':'What word is both a fruit and a color?', 'back':'orange'}, {'front':'Which band has an album named "Rubber Soul?"', 'back':'The Beatles'}, {'front':'Who stars in "The Hunger Games"?', 'back':'Jennifer Lawrence'}, {'front':'What city is the capital of Ecuador?', 'back':'quito'}];
 
 var makeDeck = function(sourceArray){
 	for (i = 0; i < sourceArray.length; i++){
@@ -22,6 +24,46 @@ var makeDeck = function(sourceArray){
 	};
 	return deck;
 };
+
+var flip = function(currentCard){
+	console.log(currentCard.back);
+};
+
+var compare = function(){
+	inquirer.prompt([
+	{
+    type: "text",
+    name: "userGuess",
+    message: "Type in your guess!",
+    }
+]).then(function(userGuess, currentCard) {
+	console.log(userGuess);
+	console.log("currentCard:" + currentCard)
+	if(userGuess === currentCard.back){
+		console.log("Correct!")
+	}
+   });
+};
+
+
+
+
+startQuiz = function(deck){
+	//code to get a card
+	var length = deck.length
+	for (i = 0; i < length; i++){
+		currentCard = deck[i];
+		//var userGuess = process.argV[3];
+		};
+		console.log(currentCard.front);
+		// inquirer.prompt([this.front]).then(function(userGuess) {
+		// 	console.log("user guess: " + userGuess)
+		// 	//write if statement for if user answer === this.back
+		// 	//if
+		// 	});
+		// 	console.log(currentCard.front);
+		
+	};
 
 // runs inquirer and asks the user a series of questions whose replies are stored within the variable answers inside of the .then statement.
 inquirer.prompt([
@@ -43,14 +85,9 @@ inquirer.prompt([
 			  if (choices.userSelection[0] === 'Music Flashcards Quiz') {
 			  	 console.log('Music Quiz');
 			  	 deck = makeDeck(musicSources);
+			  	 startQuiz(musicSources);
 			  	 cardAction();
-			  	  	//function that asks all questions inside of deck and checks the answer
-	  	  	//var startQuiz = function (deck) {
-	  	  		// for (card in deck){
-	  	  		// 	console.log(deck[card].front);
-	  	  		// 	console.log(deck[card].back);
-	  	  		// };
-	  	  		//}
+			
 	  	  	//startQuiz(deck);
 	  	  } else if (choices.userSelection[0] === 'National Parks Flashcards Quiz') {
 	  	  	console.log('National Parks Quiz');
@@ -68,6 +105,35 @@ inquirer.prompt([
 		  }
 	});
 
+var cardAction = function(){
+		inquirer.prompt([
+			{
+		    type: "checkbox",
+		    name: "userSelection",
+		    message: "What would you like to do with the card?",
+		    choices: [
+		    'answer', 
+		    'flip', 
+		    'next',  
+		    'exit']
+			}
+		]).then(function(choices) {
+			if (choices.userSelection[0] === 'answer') {
+			   //console.log('Please enter your guess.');
+			   compare();
+			  	 //function that shows front of card
+			 } else if (choices.userSelection[0] === 'flip') {
+			  	 //console.log('flip');
+			  	 flip(currentCard);
+			  	 //function that shows the front of card
+			 } else if (choices.userSelection[0] === 'next') {
+			  	 console.log('next');
+			  	 //function that displays next card in the array
+			 } else if (choices.userSelection[0] === 'exit') {
+			  	 console.log('Thanks for playing!');
+			}
+		 });
+	};
 
 //an arry of objects to be used to create the flashcards
 
@@ -83,21 +149,15 @@ inquirer.prompt([
 // console.log(card);
 //functions to call to read cards and do quizzes
 
-startQuiz = function(deck){
-	//code to get a card
-	var length = deck.length
-	for (i = 0; i < length; i++){
-		var currentCard = deck.pop()
-		var userGuess = process.argV[3];
-		inquirer.prompt([this.front]).then(function(userGuess) {
-			console.log("user guess: " + userGuess)
-			//write if statement for if user answer === this.back
-			//if
-			});
-			console.log(currentCard.front);
-		};
-	};
 
+
+  	  	//function that asks all questions inside of deck and checks the answer
+	  	  	// var startQuiz = function (deck) {
+	  	  	// 	for (card in deck){
+	  	  	// 		console.log(deck[card].front);
+	  	  	// 		console.log(deck[card].back);
+	  	  	// 	};
+	  	  	// 	}
 	 
 
 
@@ -106,33 +166,7 @@ startQuiz = function(deck){
     //     console.log(data);
     // });
 
-    var cardAction = function(){
-		inquirer.prompt([
-			{
-		    type: "checkbox",
-		    name: "userSelection",
-		    message: "What would you like to do with the card?",
-		    choices: [
-		    'answer', 
-		    'flip', 
-		    'next',  
-		    'exit']
-			}
-		]).then(function(choices) {
-			if (choices.userSelection[0] === 'answer') {
-			   console.log('answer');
-			  	 //function that shows front of card
-			 } else if (choices.userSelection[0] === 'flip') {
-			  	 console.log('flip');
-			  	 //function that shows the front of card
-			 } else if (choices.userSelection[0] === 'next') {
-			  	 console.log('next');
-			  	 //function that displays next card in the array
-			 } else if (choices.userSelection[0] === 'exit') {
-			  	 console.log('Thanks for playing!');
-			}
-		 });
-	};
+    
 
 
 
